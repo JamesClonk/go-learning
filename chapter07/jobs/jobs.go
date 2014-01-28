1523c73e8f673cbb6975b07378259b3132fc6e41
JamesClonk <jamesclonk@jamesclonk.ch>
2014-01-28 16:06:07 +0100

    started chapter 07

---
+++ chapter07/jobs/jobs.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	jobSample()
+}
+
+type Job struct {
+	Value int
+}
+
+func jobSample() {
+	joblist := []Job{{1}, {2}, {3}, {4}, {5}}
+
+	jobs := make(chan Job)
+	done := make(chan bool, len(joblist))
+
+	go func() {
+		for _, job := range joblist {
+			jobs <- job // blocks waiting for a receive, because channel is unbuffered (size 0)
+		}
+		close(jobs)
+	}()
+
+	go func() {
+		for job := range jobs { // blocks waiting for a send
+			fmt.Printf("Job: %d\n", job.Value) // "do" the job
+			done <- true
+		}
+	}()
+
+	for i := 0; i < len(joblist); i++ {
+		<-done // blocks waiting for a receive
+	}
+}
